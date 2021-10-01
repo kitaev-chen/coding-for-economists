668c98a828be6d9d7b68f17187aed8401306485f
aeturrell <aeturrell@users.noreply.github.com>
2021-10-01 06:43:18 +0000

    poetry conda hybrid :-/

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,9 @@
 # to install non Python packages in linux envs
 sudo apt-get install graphviz graphviz-dev
 
+# install the env
+poetry install
+
 # name the env
 poetry run python3.8 -m ipykernel install --user --name=codeforecon
 
@@ -8,3 +11,13 @@ poetry run python3.8 -m ipykernel install --user --name=codeforecon
 
 # download the spacy models
 poetry run python3.8 -m spacy download en_core_web_sm
+
+# use conda for what can't be done otherwise
+conda init
+
+# replace shell
+exec bash
+
+# conda packages
+sudo conda install -y osmnx
+sudo conda install -y plotly
